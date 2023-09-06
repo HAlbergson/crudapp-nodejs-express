4d61658d9eed9958c5b539717880e599f53b2506
HAlbergson <hjalte3480@gmail.com>
2023-09-06 17:27:27 +0200

    ytguyhuijl

--- front-end/app.js
+++ front-end/app.js
@@ -1,14 +1,17 @@
-import { getData, createArtist } from "./rest-service.js";
+import { getArtists, updateArtist, createArtist } from "./rest-service.js";
 
 window.addEventListener("load", initApp);
+
+let artists;
 
 function initApp() {
   updateGrid();
   document.querySelector("#create-artist-btn").addEventListener("click", showCreateArtistDialog);
   document.querySelector("#form-create-artist").addEventListener("submit", createArtistClicked);
+  //   document.querySelector("#form-update-artist").addEventListener("submit", updateArtistClicked);
 }
 async function updateGrid() {
-  const artists = await getData();
+  artists = await getArtists();
   displayArtists(artists);
 }
 
@@ -32,11 +35,27 @@ function displayArtists(list) {
             </div>
         </article>`;
     document.querySelector("#artist-grid").insertAdjacentHTML("beforeend", artistHTML);
+    document.querySelector("#artist-grid article:last-child .update-btn").addEventListener("click", updateClicked);
   }
 }
 function showCreateArtistDialog() {
-  document.querySelector("#dialog-create-champ").showModal();
+  document.querySelector("#dialog-create-artist").showModal();
 }
+
+function updateClicked() {
+  const updateForm = document.querySelector("#form-update-artist");
+  updateForm.name.value = artists.name;
+  updateForm.birthdate.value = artists.birthdate;
+  updateForm.activeSince.value = artists.activeSince;
+  updateForm.genres.value = artists.genres;
+  updateForm.labels.value = artists.labels;
+  updateForm.website.value = artists.website;
+  updateForm.image.value = artists.image;
+  updateForm.shortDescription.value = artists.shortDescription;
+  updateForm.setAttribute("artist-id", artists.id);
+  document.querySelector("#dialog-update-artist").showModal();
+}
+
 async function createArtistClicked(event) {
   const form = event.target;
   const name = form.name.value;
@@ -49,6 +68,23 @@ async function createArtistClicked(event) {
   const shortDescription = form.shortDescription.value;
 
   const response = await createArtist(name, birthdate, activeSince, genres, labels, website, image, shortDescription);
+  if (response.ok) {
+    updateGrid();
+    form.reset();
+  }
+}
+async function updateArtistClicked(event) {
+  const form = event.target;
+  const name = form.name.value;
+  const birthdate = form.birthdate.value;
+  const activeSince = form.activeSince.value;
+  const genres = form.genres.value;
+  const labels = form.labels.value;
+  const website = form.website.value;
+  const image = form.image.value;
+  const shortDescription = form.shortDescription.value;
+  const id = form.getAttribute("artist-id");
+  const response = await updateArtist(id, name, birthdate, activeSince, genres, labels, website, image, shortDescription);
   if (response.ok) {
     updateGrid();
     form.reset();
--- front-end/rest-service.js
+++ front-end/rest-service.js
@@ -1,7 +1,8 @@
-const endpoint = "http://localhost:3333/artists";
+const endpoint = "http://localhost:3333";
+const headers = { "Content-Type": "application/json" };
 
-async function getData() {
-  const response = await fetch(`${endpoint}`);
+async function getArtists() {
+  const response = await fetch(`${endpoint}/artists`);
   const data = await response.json();
   return data;
 }
@@ -18,11 +19,51 @@ async function createArtist(name, birthdate, activeSince, genres, labels, websit
     shortDescription,
   };
   const artistJSON = JSON.stringify(newArtist);
-  const response = await fetch("", {
+  const response = await fetch(`${endpoint}/artists`, {
+    headers: headers,
     method: "POST",
     body: artistJSON,
   });
   return response;
 }
-
-export { getData, createArtist };
+async function updateArtist(id, name, birthdate, activeSince, genres, labels, website, image, shortDescription) {
+  const updateArtist = {
+    name,
+    birthdate,
+    activeSince,
+    genres,
+    labels,
+    website,
+    image,
+    shortDescription,
+  };
+  const artistJSON = JSON.stringify(updateArtist);
+  const response = await fetch(`${endpoint}/artists/${id}`, {
+    headers: headers,
+    method: "PUT",
+    body: artistJSON,
+  });
+  return response;
+}
+// async function delelteArtist(name, birthdate, activeSince, genres, labels, website, image, shortDescription) {
+//   const newArtist = {
+//     name,
+//     birthdate,
+//     activeSince,
+//     genres,
+//     labels,
+//     website,
+//     image,
+//     shortDescription,
+//   };
+//   const artistJSON = JSON.stringify(newArtist);
+//   const response = await fetch(`${endpoint}/artists`, {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     method: "DELETE",
+//     body: artistJSON,
+//   });
+//   return response;
+// }
+export { getArtists, createArtist, updateArtist };
